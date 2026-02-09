f3ce955f161ba703da7275ef9ee8e2999d16d417
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-09 19:23:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1733257049-16202",
+  "appId": "1:776682877969:web:df1822bdc7459f71b3edb0",
+  "apiKey": "AIzaSyCLFHpLq4XIia5KsKOag_LogLnSqyeVCZI",
+  "authDomain": "studio-1733257049-16202.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "776682877969"
+};
