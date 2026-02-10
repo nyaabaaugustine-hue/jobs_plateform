cbee34382685b66566ae261caa4dad70bfa83406
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-10 02:30:37 +0000

    make big image into a sliider populated

--- src/components/hero-slider.tsx
+++ src/components/hero-slider.tsx
@@ -3,6 +3,7 @@
 
 import * as React from 'react';
 import Image from 'next/image';
+import Autoplay from "embla-carousel-autoplay";
 import {
   Carousel,
   CarouselContent,
@@ -17,9 +18,16 @@ export function HeroSlider() {
     (img) => img.id.startsWith('hero-banner-')
   );
 
+  const plugin = React.useRef(
+    Autoplay({ delay: 4000, stopOnInteraction: true })
+  )
+
   return (
     <Carousel
+      plugins={[plugin.current]}
       className="w-full max-w-lg"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
     >
       <CarouselContent>
         {heroBanners.map((banner, index) => (
