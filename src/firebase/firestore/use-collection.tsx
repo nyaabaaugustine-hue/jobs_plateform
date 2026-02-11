17b3d3bfa3669045342bc4aeb88ea1252409912c
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-11 12:47:15 +0000

    Try fixing this error: `Runtime Error: undefined. Error source: Call Sta

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -86,10 +86,14 @@ export function useCollection<T = any>(
       },
       (error: FirestoreError) => {
         // This logic extracts the path from either a ref or a query
-        const path: string =
-          memoizedTargetRefOrQuery.type === 'collection'
-            ? (memoizedTargetRefOrQuery as CollectionReference).path
-            : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
+        let path: string;
+        // A CollectionReference has a public `path` property, which is the safest way to get it.
+        // A general Query object does not have this public property, so we fall back to an internal property.
+        if ((memoizedTargetRefOrQuery as CollectionReference).path) {
+            path = (memoizedTargetRefOrQuery as CollectionReference).path;
+        } else {
+            path = (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString();
+        }
 
         const contextualError = new FirestorePermissionError({
           operation: 'list',
