e45e3e5c4715ea0609bac9ec2af078c4e309ab0b
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-12 07:09:13 +0000

    invitation notification is not visible

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-        vibrant: "vibrant group border-transparent bg-black/80 backdrop-blur-sm text-primary-foreground",
+        vibrant: "vibrant group border-transparent bg-black/80 backdrop-blur-sm text-white",
       },
     },
     defaultVariants: {
