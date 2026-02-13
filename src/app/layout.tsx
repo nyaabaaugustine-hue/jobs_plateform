a492390df66b8f268addcba7be4aa2d83aec0e10
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-13 22:56:46 +0000

    default black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${spaceGrotesk.variable} font-body antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
