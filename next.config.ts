f4358e01fda0022a165e431941f460a64d171e06
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-13 13:46:23 +0000

    Perfect — since this is a React jobs platform, we can make a step-by-ste

--- next.config.ts
+++ next.config.ts
@@ -11,12 +11,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
