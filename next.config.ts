8984997953c845bcc80301bc67c3902775c927d7
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-13 12:29:47 +0000

    restore images

--- next.config.ts
+++ next.config.ts
@@ -34,9 +34,17 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
 };
 
 export default nextConfig;
+
+    
