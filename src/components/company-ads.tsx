38adac2ee8f66c58a12e8eaece08d2684303fb29
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-16 14:19:02 +0000

    above fix not working oo

--- src/components/company-ads.tsx
+++ src/components/company-ads.tsx
@@ -59,7 +59,7 @@ const validAds = adData.map(ad => {
 
 export default function CompanyAds() {
   const plugin = useRef(
-    Autoplay({ delay: 5000, stopOnInteraction: true })
+    Autoplay({ delay: 5000, stopOnInteraction: false, stopOnMouseEnter: true })
   );
 
   // This is the "error test": If no valid ads can be constructed, show an error message.
@@ -94,8 +94,6 @@ export default function CompanyAds() {
           }}
           plugins={[plugin.current]}
           className="relative"
-          onMouseEnter={plugin.current.stop}
-          onMouseLeave={plugin.current.reset}
         >
           <CarouselContent>
             {validAds.map((ad, index) => {
