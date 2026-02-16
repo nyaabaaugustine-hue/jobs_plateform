e2a3663b0803b89de593c6128f99b4474cfa4b19
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-16 20:17:26 +0000

    change this - TGNE Solutions to color burgundy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,7 @@ export default {
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        burgundy: 'hsl(var(--burgundy))',
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
