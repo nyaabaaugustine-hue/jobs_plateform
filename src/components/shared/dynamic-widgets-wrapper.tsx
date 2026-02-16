17c5333196c4a03dc440949ce03bd925d4d2bf64
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-16 13:05:44 +0000

    1 of 1 error
    Unhandled Runtime Error
    ChunkLoadError: Loading chunk _app

--- src/components/shared/dynamic-widgets-wrapper.tsx
+++ src/components/shared/dynamic-widgets-wrapper.tsx
@@ -1,11 +1,18 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const ClientWidgets = dynamic(() => import('@/components/shared/client-widgets'), {
-  ssr: false,
-});
+import React from 'react';
+import ClientWidgets from '@/components/shared/client-widgets';
 
 export default function DynamicWidgetsWrapper() {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return <ClientWidgets />;
 }
