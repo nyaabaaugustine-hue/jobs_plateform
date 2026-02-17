c667e56f818cbfdf387c3a96db0bc43a87f2cc33
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-17 19:04:19 +0000

    favicon image -https://res.cloudinary.com/dwsl2ktt2/image/upload/v177110

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: 'Chapel Hill - Find Your Next Job',
   description: 'The Easiest Way to Get Your New Job',
   icons: {
-    icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cmVjdCB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHJ4PSI0IiBmaWxsPSIjM0I4MkY2Ii8+PHRleHQgeD0iNTAlIiB5PSI1MCUiIGRvbWluYW50LWJhc2VsaW5lPSJtaWRkbGUiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGZvbnQtZmFtaWx5PSJzYW5zLXNlcmlmIiBmb250LXNpemU9IjE2IiBmb250LXdlaWdodD0iYm9sZCIgZmlsbD0iI2ZmZiI+Q0g8L3RleHQ+PC9zdmc+',
+    icon: 'https://res.cloudinary.com/dwsl2ktt2/image/upload/v1771103002/klo_vappbs.png',
   },
 };
 
