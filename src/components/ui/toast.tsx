386ccadb78dfaf1b25706f23b4e93fe67e6fd915
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-18 09:41:44 +0000

    bring hired flash mesage down to the level og button , , placed neatly o

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-0 right-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-24 sm:right-6 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed bottom-0 right-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-6 sm:right-24 sm:top-auto sm:flex-col md:max-w-[380px]",
       className
     )}
     {...props}
